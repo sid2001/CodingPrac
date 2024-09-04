5a23ac9f1787ace80c7b50bdebbb6e5a64704b75
siddharth <sidharth2001.lumia@gmail.com>
2024-09-05 00:54:25 +0530

    g

--- cses/speedrun/DP/bookShop.cpp
+++ cses/speedrun/DP/bookShop.cpp
@@ -2,62 +2,34 @@
 #include <vector>
 #include <algorithm>
 
-void maxPages(std::vector<std::pair<int,int>> vec,  int rem, int x,int curr_p ,int &max_p,std::vector<int> &tab, int idx = 0) {
-  //std::cout<<"x: "<<x<<" curr_p: "<<curr_p<<" tab[x]: "<<tab[x]<<" max_p: "<<max_p<<" rem: "<<rem<<" price[idx]: "<<price[idx]<<"\n";
-  
-  if(idx >= vec.size() && x >= 0) {
-      max_p = std::max(max_p, curr_p);
-      return;
+using namespace std;
+
+int solve(vector<pair<int,int>> &book, vector<int> &page, vector<vector<int>> &dp, int x) {
+  for(int i = 1; i < dp.size(); i++) {
+    for(int j = 1; j <= x; j++){
+      if(book[i-1].first<=j) {
+        dp[i][j] = max(dp[i-1][j-book[i-1].first]+ book[i-1].second,dp[i-1][j]);
+      } else {
+        dp[i][j] = dp[i-1][j];
+      }
     }
-  else if(x < 0 ) return;
-  else if(tab[x] > curr_p) return;
-  else if(curr_p + rem < max_p) return;
-  
-  if(curr_p > max_p) {
-    max_p = curr_p;
   }
-  
-  tab[x] = curr_p;
 
-  //std::cout<<curr_p<<"\n";
-  maxPages(vec, rem-vec[idx].second, x-vec[idx].first, curr_p+vec[idx].second, max_p,tab,idx+1);
-  maxPages(vec, rem-vec[idx].second,x,curr_p,max_p,tab, idx+1);
-  return;
+  return dp[dp.size()-1][x];
 }
 
-
-
-int solve(std::vector<std::pair<int,int>> vec, int x,int total_pages) {
-  int rem = total_pages;
-  int max_p = 0;
-  std::vector<int> tab(x+1,0);
-  maxPages(vec, rem, x,0,max_p,tab);
-
-  return max_p;
-}
-
-int comparefn(std::pair<int,int> l1, std::pair<int,int> l2) {
-  if((float)l1.second/(float)l1.first > (float)l2.first/(float)l2.second) return 0;
-  else return 1;
-}
 int main() {
   int n, x;
-  std::cin>>n>>x;
-  std::vector<std::pair<int,int>> vec(n);
-  int total_pages = 0;
-  int y;
-  for(int i=0; i<n; i++) {
-    std::cin>>y;
-    vec[i] = {y,0};
+  cin>>n>>x;
+  vector<pair<int,int>> book(n,{0,0});
+  vector<int> page(n,0);
+  vector<vector<int>> dp(n+1,vector<int>(x+1,0));
+  for(int i = 0;i<n;i++){
+    cin>>book[i].first;
   }
-  for(int i = 0; i<n; i++) {
-    std::cin>>y;
-    total_pages += y;
-    vec[i].second = y; 
+  for(int i = 0; i < n; i++) {
+    cin>>book[i].second;
   }
-  std::sort(vec.begin(),vec.end(),comparefn);
-  //display(vec);
-  std::cout<<solve(vec,x,total_pages)<<"\n";
-
-  return 0;
+  sort(book.begin(),book.end());
+  cout<<solve(book, page, dp,x);
 }
